9ab708b8a60d43d0846b624f0dbcc4747f2e0343
leandrowakim <leandrowakim@gmail.com>
2021-05-07 11:33:07 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -13,7 +13,7 @@ import com.devsuperior.dsvendas.service.SellerService;
 
 @RestController
 @RequestMapping(value = "/sellers")
-public class ServiceController {
+public class SellerController {
 
 	@Autowired
 	private SellerService service;
